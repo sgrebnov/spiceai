9720c590c4a4cda23efd7b446ed6f613e10b90ce
Sergei Grebnov <sergei.grebnov@gmail.com>
2024-11-07 12:26:52 -0800

    Add integration tests for Search API (OpenAI and HF models) (#3410)
    * Add integration tests Search API (OpenAI and HF models)
    
    * Add snapshots
    
    * update

--- crates/runtime/tests/models/mod.rs
+++ crates/runtime/tests/models/mod.rs
@@ -15,12 +15,13 @@ limitations under the License.
 */
 
 use reqwest::Client;
-use serde_json::json;
 use spicepod::component::{
     dataset::{acceleration::Acceleration, Dataset},
     params::Params,
 };
 
+use serde_json::{json, Value};
+mod hf;
 mod openai;
 
 fn get_taxi_trips_dataset() -> Dataset {
@@ -41,13 +42,46 @@ fn get_taxi_trips_dataset() -> Dataset {
     dataset
 }
 
+fn get_tpcds_dataset(ds_name: &str) -> Dataset {
+    let mut dataset = Dataset::new(
+        format!("s3://spiceai-public-datasets/tpcds/{ds_name}/"),
+        ds_name,
+    );
+    dataset.params = Some(Params::from_string_map(
+        vec![
+            ("file_format".to_string(), "parquet".to_string()),
+            ("client_timeout".to_string(), "120s".to_string()),
+        ]
+        .into_iter()
+        .collect(),
+    ));
+    dataset.acceleration = Some(Acceleration {
+        enabled: true,
+        refresh_sql: Some(format!("SELECT * FROM {ds_name} LIMIT 20")),
+        ..Default::default()
+    });
+    dataset
+}
+
+fn json_is_single_row_with_value(json: &Value, expected_value: i64) -> bool {
+    json.as_array()
+        .filter(|array| array.len() == 1)
+        .and_then(|array| array.first())
+        .map_or(false, |item| {
+            item.as_object().map_or(false, |map| {
+                map.values()
+                    .any(|value| value == &Value::from(expected_value))
+            })
+        })
+}
+
 async fn send_nsql_request(
     base_url: &str,
     query: &str,
     model: Option<&str>,
     sample_data_enabled: Option<bool>,
     datasets: Option<Vec<String>>,
-) -> Result<String, reqwest::Error> {
+) -> Result<Value, reqwest::Error> {
     let mut request_body = json!({
         "query": query,
     });
@@ -61,13 +95,72 @@ async fn send_nsql_request(
     if let Some(ds) = datasets {
         request_body["datasets"] = json!(ds);
     }
-    Client::new()
+    let response = Client::new()
         .post(format!("{base_url}/v1/nsql"))
         .header("Content-Type", "application/json")
         .json(&request_body)
         .send()
         .await?
         .error_for_status()?
-        .text()
-        .await
+        .json::<Value>()
+        .await?;
+
+    Ok(response)
+}
+
+async fn send_search_request(
+    base_url: &str,
+    text: &str,
+    limit: Option<usize>,
+    datasets: Option<Vec<String>>,
+    where_cond: Option<&str>,
+    additional_columns: Option<Vec<String>>,
+) -> Result<Value, reqwest::Error> {
+    let mut request_body = json!({
+        "text": text,
+    });
+
+    if let Some(limit) = limit {
+        request_body["limit"] = json!(limit);
+    }
+
+    if let Some(ds) = datasets {
+        request_body["datasets"] = json!(ds);
+    }
+
+    if let Some(where_cond) = where_cond {
+        request_body["where_cond"] = json!(where_cond);
+    }
+
+    if let Some(columns) = additional_columns {
+        request_body["additional_columns"] = json!(columns);
+    }
+
+    let response = Client::new()
+        .post(format!("{base_url}/v1/search"))
+        .header("Content-Type", "application/json")
+        .json(&request_body)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Value>()
+        .await?;
+
+    Ok(response)
+}
+
+fn normalize_search_response(mut json: Value) -> String {
+    if let Some(matches) = json.get_mut("matches").and_then(|m| m.as_array_mut()) {
+        for m in matches {
+            if let Some(score) = m.get_mut("score") {
+                *score = json!("score_val");
+            }
+        }
+    }
+
+    if let Some(duration) = json.get_mut("duration_ms") {
+        *duration = json!("duration_ms_val");
+    }
+
+    json.to_string()
 }
